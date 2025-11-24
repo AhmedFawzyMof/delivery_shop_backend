8e84774f3e42c58129d358150f50ea50871ebbfc
AhmedFawzyMof <np1265448@gmail.com>
2025-11-24 21:09:32 +0200

    fix: login with phone number

--- public/assets/CardDescription.vue_vue_type_script_setup_true_lang-BzARtVmM.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./auth-BOWgCVVb.js";import{d as r,e as t,o as n,n as c,u as p,r as l}from"./index-d-g9mRbR.js";const u=r({__name:"CardDescription",props:{class:{}},setup(s){const e=s;return(a,m)=>(n(),t("p",{"data-slot":"card-description",class:c(p(o)("text-muted-foreground text-sm",e.class))},[l(a.$slots,"default")],2))}});export{u as _};
